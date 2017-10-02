77d03489ae5f35779bdb4a2494773fb5bde98583
Mike Moran <mmoran0032@gmail.com>
2017-10-01 20:41:29 -0400

    Base template and error handling

--- hello.py
+++ hello.py
@@ -2,9 +2,11 @@
 
 
 from flask import Flask, render_template, request
-from flask.ext.script import Manager
+from flask_bootstrap import Bootstrap
+from flask_script import Manager
 
 app = Flask(__name__)
+bootstrap = Bootstrap(app)
 manager = Manager(app)
 
 
@@ -17,6 +19,17 @@ def index():
 @app.route('/user/<name>')
 def user(name):
     return render_template('user.html', name=name)
+
+
+# error handlers
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html'), 500
 
 
 if __name__ == '__main__':
