dd054c85bbfffbf9a932fd1393afd5c03755046e
Mike Moran <mmoran0032@gmail.com>
2017-10-01 20:53:39 -0400

    Flask-moment (localized times)

--- hello.py
+++ hello.py
@@ -1,19 +1,25 @@
 #!/usr/bin/env python3
 
 
+from datetime import datetime
+
 from flask import Flask, render_template, request
 from flask_bootstrap import Bootstrap
+from flask_moment import Moment
 from flask_script import Manager
 
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
 manager = Manager(app)
+moment = Moment(app)
 
 
 @app.route('/')
 def index():
     user_agent = request.headers.get('User-Agent')
-    return render_template('index.html', user_agent=user_agent)
+    return render_template('index.html',
+                           user_agent=user_agent,
+                           current_time=datetime.utcnow())
 
 
 @app.route('/user/<name>')
