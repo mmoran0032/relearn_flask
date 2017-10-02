a285fdd179a687ede21fc230a720e632e33a1b20
Mike Moran <mmoran0032@gmail.com>
2017-10-01 21:34:30 -0400

    Better form handling

--- hello.py
+++ hello.py
@@ -3,23 +3,41 @@
 
 from datetime import datetime
 
-from flask import Flask, render_template, request
+from flask import Flask, redirect, render_template, request, session, url_for
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from flask_script import Manager
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import Required
+
+
+class NameForm(FlaskForm):
+    name = StringField('Name:', validators=[Required()])
+    submit = SubmitField('Submit')
+
 
 app = Flask(__name__)
+with open('secret_key.txt', 'r') as f:
+    app.config['SECRET_KEY'] = f.read().strip()
+
 bootstrap = Bootstrap(app)
 manager = Manager(app)
 moment = Moment(app)
 
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
+    form = NameForm()
+    if form.validate_on_submit():
+        session['name'] = form.name.data
+        return redirect(url_for('index'))
     user_agent = request.headers.get('User-Agent')
     return render_template('index.html',
                            user_agent=user_agent,
-                           current_time=datetime.utcnow())
+                           current_time=datetime.utcnow(),
+                           form=form,
+                           name=session.get('name'))
 
 
 @app.route('/user/<name>')
